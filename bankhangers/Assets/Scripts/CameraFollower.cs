4cd068ffe8725b011e1dd18d5fcfb51cdda97ced
Selawen <37039183+Selawen@users.noreply.github.com>
2023-03-02 13:25:07 +0100

    Added camera follow
    - made camera follow player
    - added first and third person modes
    - Added simple scene dressing to see where you are

---
+++ bankhangers/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    [SerializeField] private bool firstPerson;
+
+    [Header("First Person")]
+    [SerializeField] private Vector3 headOffset;
+
+    [Header("Third Person")]
+    [SerializeField] private float followSpeed;
+    [SerializeField] private Vector3 followOffset;
+    [SerializeField] private Quaternion rotation;
+
+    private void Awake()
+    {
+        if (firstPerson)
+        {
+            transform.parent = player;
+            transform.SetPositionAndRotation(Vector3.zero+headOffset, Quaternion.identity);
+        }
+        else
+        {
+            transform.position = player.position + followOffset;
+            transform.rotation = rotation;
+        }
+    }
+
+    private void Update()
+    {
+        if (!firstPerson)
+        {
+            Vector3 direction = (player.position + followOffset) - transform.position;
+
+            transform.Translate(direction * followSpeed * Time.deltaTime);
+        }
+    }
+}
