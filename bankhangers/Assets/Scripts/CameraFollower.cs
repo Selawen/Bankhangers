a73922099bd370c7986502c417bb17253fc47394
JS07\Projectadm <inger.smit12@gmail.com>
2023-03-03 15:52:53 +0100

    - Added one more movement option - played with camera modes/movement

--- bankhangers/Assets/Scripts/CameraFollower.cs
+++ bankhangers/Assets/Scripts/CameraFollower.cs
@@ -6,7 +6,9 @@ public class CameraFollower : MonoBehaviour
 {
     [SerializeField] private Transform player;
 
-    [SerializeField] private bool firstPerson;
+    [SerializeField] private bool firstPerson, isometrisch, zoom;
+
+    [SerializeField] private float minZoom, maxZoom;
 
     [Header("First Person")]
     [SerializeField] private Vector3 headOffset;
@@ -16,10 +18,15 @@ public class CameraFollower : MonoBehaviour
     [SerializeField] private Vector3 followOffset;
     [SerializeField] private Quaternion rotation;
 
+    private Vector3 lastMouse;
+    private float currentZoom = 8;
+
     private void OnValidate()
     {
         if (firstPerson)
         {
+            isometrisch = false;
+            zoom = false;
             transform.parent = player;
             transform.SetPositionAndRotation(Vector3.zero+headOffset, Quaternion.identity);
         }
@@ -28,12 +35,35 @@ public class CameraFollower : MonoBehaviour
             transform.parent = null;
             transform.position = player.position + followOffset;
             transform.rotation = rotation;
-        }
+
+        } 
+        
+        if (isometrisch)
+            Camera.main.orthographic = true;
+        else
+            Camera.main.orthographic = false;
+
     }
 
     private void Update()
     {
-        if (!firstPerson)
+        if (zoom)
+        {
+            currentZoom -= Input.mouseScrollDelta.y;
+            currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+            if (isometrisch)
+            {
+                Camera.main.orthographicSize = currentZoom;
+            }
+            else
+            {
+                //transform.LookAt(player);
+                transform.position = player.position - transform.forward * currentZoom;
+            }
+        }
+
+        if (!firstPerson && !zoom)
         {
             Vector3 direction = (player.position + followOffset) - transform.position;
 
